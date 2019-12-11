cb21ad3cc325008cc3faa547d01d381870e76a82
mr2214 <57703674+mr2214@users.noreply.github.com>
2019-12-11 09:49:25 +0100

    Add files via upload

---
+++ data_for_presentation.R
@@ -0,0 +1,3 @@
+colnames(mean.monthly.precipitation) <- c("month","mean precipitation")
+colnames(data.mean.dry.bulb.temperature) <- c('Mean Value','Mean Daily Value', 'Standard Deviation of Mean Daily Value', 'Mean Daily Maximum Value', 'Mean Daily Minimum Value')
+colnames(max.priciptation.24.hours) <- c("month","max percipitation")
